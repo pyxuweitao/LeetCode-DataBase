710633fce7cdfa74fccd24928b36254f051011a3
weitao.xu <weitao.xu@sklcc.cc>
2015-04-25 15:26:46 +0800

    182

--- 182-Duplicate-Emails.sql
+++ 182-Duplicate-Emails.sql
@@ -1,5 +1,5 @@
 ---Runtime:1120 ms
 SELECT DISTINCT(A.Email) FROM Person A JOIN Person B ON A.EMAIL = B.EMAIL AND A.ID != B.ID;
 
----Runtime:1092 ms
-SELECT DISTINCT(Email) FROM Person GROUP BY Email HAVING COUNT(*) > 1;
+---Runtime:990 ms
+SELECT Email FROM Person GROUP BY Email HAVING COUNT(*) > 1;
