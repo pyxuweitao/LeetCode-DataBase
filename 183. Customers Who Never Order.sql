c01df9b2c67342e35c5635ed136dd25bef9eb8d2
XuWeitao <xuweitao.py@hotmail.com>
2016-05-04 15:23:47 +0800

    增加183为markdown并添加题目内容

--- 183. Customers Who Never Order.sql
+++
@@ -1,9 +0,0 @@
-#699ms
-SELECT a.Name From Customers a LEFT JOIN Orders b
-ON a.Id = b.CustomerId
-WHERE b.Id is null;
-
-#738ms
-SELECT a.Name From Customers a WHERE a.Id not in (
-    SELECT CustomerId FROM Orders );
-
