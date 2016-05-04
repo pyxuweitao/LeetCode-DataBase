451d9be9d24ed39b4c61073b176d52ebd179fb7a
XuWeitao <xuweitao.py@hotmail.com>
2016-05-04 15:19:40 +0800

    182改为Markdown，增加题目内容

--- 182-Duplicate-Emails.sql
+++
@@ -1,5 +0,0 @@
-#Runtime:1120 ms
-SELECT DISTINCT(A.Email) FROM Person A JOIN Person B ON A.EMAIL = B.EMAIL AND A.ID != B.ID;
-
-#Runtime:990 ms
-SELECT Email FROM Person GROUP BY Email HAVING COUNT(*) > 1;
